a7bca0b8b4c99c059f9b291eef68c3e7d4648486
tapsey <matangirataps@gmail.com>
2019-11-07 10:30:10 +0200

    Task 3 - Launch the Phone Activity

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -41,4 +41,11 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+    public void createPhoneIntent(View view){
+        Intent callIntent = new Intent(Intent.ACTION_DIAL);
+        Uri uri = Uri.parse("tel:0123456789");
+        callIntent.setData(uri);
+        startActivity(callIntent);
+    }
+
 }
