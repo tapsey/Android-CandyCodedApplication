ada4ee33ba40e7c01540185cdec122d5e34ce938
tapsey <matangirataps@gmail.com>
2019-11-07 10:16:46 +0200

    Task 1 - Show Store Information Activity I-1

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -87,10 +87,8 @@ public class MainActivity extends AppCompatActivity {
     // ***
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId() ==  R.id.info){
-            Intent intent =new Intent(this, InfoActivity.class);
-            startActivity(intent);
-        }
+        Intent infoIntent =new Intent(this, InfoActivity.class);
+        startActivity(infoIntent);
         return super.onOptionsItemSelected(item);
     }
 
